c5f7183e8fb38a138f56bc6a51080021971fe660
Ece2003 <56912839+Ece2003@users.noreply.github.com>
2019-10-23 12:38:39 +0300

    Update Bölünme.py

--- Python/Bölünme.py
+++ Python/Bölünme.py
@@ -2,10 +2,10 @@
 a = int(input("1.sayı: "))
 
 # "%" operatörü bir sayısının diğer sayıya bölümünden kalan sonucu verir.
-if a%2==0:
-   print(a,"sayısı çifttir.")
+if a%3==0:
+   print(a,"3'e bölünür.")
 else:
-   print(a, "sayısı tektir.")
+   print(a, "sayı 3'e bölünmez.")
 
 #GÖREV:Yukarıda verilen örenğin benzerini üçe bölünebilme ile yapınız:
 #!!!Python programlama dilinde boşluklar programın çalışması için önem arz etmektedir.
